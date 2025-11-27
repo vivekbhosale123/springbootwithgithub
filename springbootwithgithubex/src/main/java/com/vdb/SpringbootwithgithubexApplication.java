5758782bee132ef5db91784b4ce641c1d107b76a
vivek Bhosale <vivekbhosale146@gmail.com>
2025-11-27 14:58:57 +0530

    initial commit

---
+++ springbootwithgithubex/src/main/java/com/vdb/SpringbootwithgithubexApplication.java
@@ -0,0 +1,13 @@
+package com.vdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithgithubexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithgithubexApplication.class, args);
+	}
+
+}
